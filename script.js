12b099e5e51bef1d23e0f3daf9d83727866ab689
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2022-10-21 17:02:41 -0300

    add features

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 const eye = document.getElementById('img-eye');
-const input_password = document.getElementById('input-password');
-const borde_load_password = document.getElementById('border-load-password');
-const load_password = document.getElementById('load-password');
-const input_cadastre_se_senha = document.getElementById('cadastre-se-senha');
-const placeholder_password  = document.getElementById('input-password:placeholder');
+const inputPassword = document.getElementById('input-password');
+const inputUser = document.querySelector('#input-user');
 
-eye.onmousedown = ()=>{
 
-    eye.src='img/eye-open.svg'; 
-    input_password.type = "text";  
-      
-};
 
-eye.onmouseup = ()=>{
-
-    eye.src='img/eye-close.svg';   
-    input_password.type = "password";  
-
-};
-
-input_password.oninput  = ()=>{   
+inputPassword.oninput  = () =>  {      
+    inputPassword.style = inputPassword.value != '' ? "outline: 2px solid  rgb(0, 111, 230); border: none" : "";
+    eye.style.visibility = inputPassword.value != '' ? 'visible' : 'hidden';       
     
-    input_password.style = input_password.value != '' ? "outline: 2px solid  rgb(0, 111, 230); border: none" : "";
-    eye.style.visibility = input_password.value != '' ? 'visible' : 'hidden';       
-    
+    const validatePassword = /(?=.*[}{,.^?~=+\-_\/*\-+.\|])(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}/;    
+    console.log(validatePassword.test(String(inputPassword.value)));    
 };
+
+inputUser.addEventListener("input", () => {
+    const validateUser = /[^a-zA-z0-9.]/;
+    console.log(validateUser.test(String(inputUser.value)));       
+}); 
+
+
 
 
 
@@ -32,6 +25,8 @@ input_password.oninput  = ()=>{
 const formLogin = document.querySelector("#form-login");
 const formRegister = document.querySelector("#form-register");
 const formRecoverPassword = document.querySelector("#form-recover-password");
+const formWelcome = document.querySelector('#form-welcome');
+
 
 formLogin.addEventListener("submit", (e) =>{
     e.preventDefault();
@@ -39,34 +34,49 @@ formLogin.addEventListener("submit", (e) =>{
 
 const clickAqui = document.querySelector("#click-aqui");
 const cadastreSe = document.querySelector('#cadastre-se');
-
 const xRegister = document.querySelector('#x-register');
 const xRecoverPassword = document.querySelector('#x-recover-password');
+const xWelcome = document.querySelector('#x-welcome');
+const buttonEntrar = document.querySelector('#button-enter');
 
+buttonEntrar.addEventListener("click", () => {
+    formLogin.style = "display : none";
+    setTimeout(() => {
+        formWelcome.style = "display : block";
+    },1000)
+});
 
-clickAqui.addEventListener("click", () => {
-        
+clickAqui.addEventListener("click", () => {        
     formLogin.style = "display: none";
     formRecoverPassword.style = "display : block"    
 });
 
-cadastreSe.addEventListener("click", () => {
-        
+cadastreSe.addEventListener("click", () => {        
     formLogin.style = "display: none";
     formRegister.style = "display : block"    
 });
 
-
-
-
 xRegister.addEventListener("click", () => {
     formRegister.style = "display: none";
-    formLogin.style = "display: ''";
-    
+    formLogin.style = "display: block";    
 });
-
 
 xRecoverPassword.addEventListener("click", () => {
     formRecoverPassword.style = "display : none"
-    formLogin.style = "display: ''";
+    formLogin.style = "display: block";
 });
+
+xWelcome.addEventListener("click", () => {
+    formWelcome.style = "display : none"
+    formLogin.style = "display: block";
+});
+
+eye.onmousedown = ()=>{
+    eye.src='img/eye-open.svg'; 
+    inputPassword.type = "text";        
+};
+
+eye.onmouseup = ()=>{
+    eye.src='img/eye-close.svg';   
+    inputPassword.type = "password"; 
+};
